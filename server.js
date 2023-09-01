f5659b64728bf30f8f2890aeff69538ed6b32a51
Sitraka <73686076+sitrakak@users.noreply.github.com>
2023-09-01 19:07:40 +0300

    depl

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/assignment-app/"));
+app.use(express.static(__dirname + "/dist/back-office-tpt/"));
 
 app.get("/*", function (req, res) {
- res.sendFile(path.join(__dirname + "/dist/assignment-app/index.html"));
+ res.sendFile(path.join(__dirname + "/dist/back-office-tpt/index.html"));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8081);
